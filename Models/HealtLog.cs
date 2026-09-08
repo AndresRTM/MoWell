bfdb6f34fd18482ede4bced351c6c3d7d2cb4693
AndresRTM <81742862+AndresRTM@users.noreply.github.com>
2026-09-08 17:37:09 +0200

    Remove weather API; add EF Core, Scalar, and health log model
    Removed WeatherForecastController and model. Added EF Core and Scalar.AspNetCore packages to MoWell.csproj. Updated Program.cs for Scalar API mapping. Introduced HealtLog model and LogType enum for health logging.

---
+++ Models/HealtLog.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MoWell.Models
+{
+    public class HealtLog
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public LogType Type { get; set; }
+
+        public int value { get; set; }
+
+    }
+}
+
