cd2f1f02e92a2c60bdc03d83a2f2666e06ebb94e
AndresRTM <andresdev@duck.com>
2026-09-19 23:47:15 +0200

    Added  methods to IHealtLogRepository interface

--- Interfaces/IHealtLogRepository.cs
+++ Interfaces/IHealtLogRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace MoWell.Interfaces
+﻿using MoWell.Models;
+
+namespace MoWell.Interfaces
 {
     public interface IHealtLogRepository
     {
+
+        Task<List<HealthLog>> GetAllHealthLogs(string userId);
+        Task<HealthLog> GetHealthLogById(int id);
+        Task<HealthLog> CreateHealthLog(string userId, DateTime dateTime, LogType type, int value);
+        Task<HealthLog> EditHealthLog(int id, LogType type, int value);
+        Task DeleteHealthLog(int id);
+
+
+
     }
 }
