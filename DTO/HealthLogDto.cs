170dd5fd194877f412806edb15e3a697d5209129
AndresRTM <andresdev@duck.com>
2026-09-20 19:41:02 +0200

    Added HealthLogService Interface

---
+++ DTO/HealthLogDto.cs
@@ -0,0 +1,12 @@
+﻿using MoWell.Models;
+
+namespace MoWell.DTO
+{
+    public class HealthLogDto
+    {
+        public int Id { get; set; }
+        public DateTime Time {  get; set; }
+        public LogType Type { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ DTO/HealthLogRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using MoWell.Models;
+
+namespace MoWell.DTO
+{
+    public class CreateHealthLogDto
+    {
+        public DateTime DateTime { get; set; }
+        public LogType Type { get; set; }
+        public int Value { get; set; }
+    }
+}
