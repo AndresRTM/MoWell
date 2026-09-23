28a30dea915da1277a9265d152f02093e2da86c0
AndresRTM <81742862+AndresRTM@users.noreply.github.com>
2026-09-23 15:55:23 +0200

    Improve route validation and add NotFoundException
    - Require logId >= 1 in HealthLogController routes for GetById, Edit, and Delete actions
    - Introduce NotFoundException for clearer error handling in HealthLogService
    - Add NotFoundException class under MoWell.Exceptions

--- Migrations/20260923084509_MakeNamesNullable.Designer.cs
+++ Migrations/20260923084509_MakeNamesNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MoWell.Data;
 
@@ -11,9 +12,11 @@ using MoWell.Data;
 namespace MoWell.Migrations
 {
     [DbContext(typeof(MoWellDBContext))]
-    partial class MoWellDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260923084509_MakeNamesNullable")]
+    partial class MakeNamesNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/User.cs
+++ Models/User.cs
@@ -4,8 +4,8 @@ namespace MoWell.Models
 {
     public class User : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public List<HealthLog> HealthLogs { get; set; }
     }
 }
